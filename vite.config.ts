b7d3b9cef2864fc0f19563c58422044d7452ffde
DozieMartins <m.dozie@zarttech.com>
2025-10-10 07:52:52 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // ensure build output goes here
+  },
+  server: {
+    port: 8080, // not strictly needed for build, but keeps consistency
+  },
+  base: './', // <-- important: makes sure all asset paths are relative
 })
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
